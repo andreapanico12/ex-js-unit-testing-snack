ff27571e76aee40aabd9f9f7347026c705920670
andreapanico12 <andreapanico12@gmail.com>
2025-05-20 14:26:07 +0200

    add test for findPostById function to verify correct post retrieval by ID

--- snack-vari.test.js
+++ snack-vari.test.js
@@ -50,3 +50,18 @@ test("La funzione createSlug lancia un errore se il titolo è vuoto o non valido
   const str3 = undefined;
   expect(() => createSlug(str3)).toThrow("Il titolo non può essere vuoto o non valido.");
 })
+// Snack-7
+const posts = [
+  { id: 1, title: "Post 1", slug: "Contenuto del post 1" },
+  { id: 2, title: "Post 2", slug: "Contenuto del post 2" },
+  { id: 3, title: "Post 3", slug: "Contenuto del post 3" }
+];
+
+
+test("La funzione findPostById restituisce il post corretto dato l'array di post e l'id", () => {
+  const id = 2;
+  const expected = { id: 2, title: "Post 2", slug: "Contenuto del post 2" };
+  const result = findPostById(posts, id);
+  expect(result).toEqual(expected);
+
+})
